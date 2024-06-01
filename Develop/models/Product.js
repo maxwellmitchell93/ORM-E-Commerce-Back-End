094336fbcc6f851c52732e83e599c2551a65ca93
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-31 23:57:02 -0700

    define product columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -6,7 +6,7 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// set up the fields and rules for Product model
 Product.init(
   {
     id: {
@@ -42,7 +42,7 @@ Product.init(
 
       }
     }
-    // define columns
+    // define the columns
   },
   {
     sequelize,
