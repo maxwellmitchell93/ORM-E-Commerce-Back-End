a1d73c02349d5c0b4732b829fb68480a23dcd701
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-06-01 00:01:43 -0700

    imported models,Product,Category,Tag,ProductTag

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,15 +1,15 @@
-// import models
+// import the models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs to Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
+// The Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
